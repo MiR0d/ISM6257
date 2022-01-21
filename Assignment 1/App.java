fe108d68aaa6f44fdde616f2dbafa0a25cc73b63
Michael Rodriguez <89548445+MiR0d@users.noreply.github.com>
2022-01-21 16:10:34 -0500

    Add files via upload

---
+++ Assignment 1/App.java
@@ -0,0 +1,23 @@
+/*
+Author: Michael R.
+Date Created: 01/11/2022
+Date Last Modified: 01/11/2022
+ */
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("+--------------------------------------------------------------+");
+        System.out.println("|ISOM Office                                             ####  |");
+        System.out.println("|1454 Union Road                                         ####  |");
+        System.out.println("|Gainesville, FL 32611                                   ####  |");
+        System.out.println("|                                                              |");
+        System.out.println("|                                                              |");
+        System.out.println("|                           Michael Rodriguez                  |");
+        System.out.println("|                           1902 SW 13TH ST                    |");
+        System.out.println("|                           Gainesville, FL 32608              |");
+        System.out.println("|                                                              |");
+        System.out.println("+--------------------------------------------------------------+");
+    }
+}
